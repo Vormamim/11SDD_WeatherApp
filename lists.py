814e53f9236cc91c55b79e913978c80a14ec9f2f
Vormamim <81339757+Vormamim@users.noreply.github.com>
2023-01-28 14:49:42 +1100

    Iterating through a list

---
+++ lists.py
@@ -0,0 +1,15 @@
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+# Using a for loop
+for name in names:
+    print(name)
+
+# Using a while loop
+index = 0
+while index < len(names):
+    print(names[index])
+    index += 1
+
+# Using the built-in function enumerate
+for index, name in enumerate(names):
+    print(f"{index+1}. {name}")
